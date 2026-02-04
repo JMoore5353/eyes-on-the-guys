db42572dc19fe05f169e75e2beefc8e644dfe5a1
Jacob Moore <jacobmoor2@gmail.com>
2026-02-03 20:44:15 -0700

    feat: add simulate function

--- include/eyes-on-the-guys/monte_carlo_tree_search.hpp
+++ include/eyes-on-the-guys/monte_carlo_tree_search.hpp
@@ -8,37 +8,50 @@
 namespace eyes_on_guys
 {
 
-class Node
+class Node : public std::enable_shared_from_this<Node>
 {
 public:
-  Node(int branching_factor, double exploration_bonus);
+  Node(const int id, const int branching_factor, const double exploration_bonus);
 
-  int get_best_action() const;
+  int explore_best_action() const;
+  bool has_been_visited();
+  std::shared_ptr<Node> take_action(const int action);
+  void update_count_and_action_value_function(const int best_action, const double q);
 
 private:
+  int id_;
   int branching_factor_;
   double exploration_bonus_;
+  bool node_has_been_visited_;
   Eigen::VectorXi N_s_a_;
   Eigen::VectorXd Q_s_a_;
   std::vector<std::shared_ptr<Node>> children_;
 };
 double get_ucb1_bound(const int & action, const double & exploration_bonus,
                       const Eigen::VectorXi & N_s_a, const Eigen::VectorXd & Q_s_a);
+double compute_running_average(const int new_val, const int old_val, const int new_count);
 
 class MonteCarloTreeSearch
 {
 public:
-  MonteCarloTreeSearch(int num_states, int num_actions);
+  MonteCarloTreeSearch(const int num_states, const int num_actions, const double discount_factor_);
 
-  int monte_carlo_tree_search(const int curr_state, const uint num_iter, const uint depth);
+  int monte_carlo_tree_search(const int initial_state, const uint num_iter, const uint depth,
+                              const double exploration_bonus);
 
 private:
   int num_states_;
   int num_actions_;
+  double discount_factor_;
 
-  void simulate();
-  int get_greedy_action(const int curr_state);
+  double simulate(std::shared_ptr<Node> curr_state, const uint & depth);
+  int find_greedy_action(const std::shared_ptr<Node> curr_state) const;
 };
+
+double compute_value_function_estimate(const std::shared_ptr<Node> curr_state);
+std::shared_ptr<Node> transition_from_state(std::shared_ptr<Node> curr_node, const int action);
+double compute_reward_from_transitioning(const std::shared_ptr<Node> curr_state,
+                                         const std::shared_ptr<Node> next_state);
 
 } // namespace eyes_on_guys
 
--- src/monte_carlo_tree_search.cpp
+++ src/monte_carlo_tree_search.cpp
@@ -7,22 +7,28 @@
 namespace eyes_on_guys
 {
 
-Node::Node(int branching_factor, double exploration_bonus)
-    : branching_factor_{branching_factor}
+Node::Node(const int id, const int branching_factor, const double exploration_bonus)
+    : id_{id}
+    , branching_factor_{branching_factor}
     , exploration_bonus_{exploration_bonus}
+    , node_has_been_visited_{false}
 {
   N_s_a_ = Eigen::VectorXi::Zero(branching_factor_);
   Q_s_a_ = Eigen::VectorXd::Zero(branching_factor_);
-  for (int i{0}; i < branching_factor_; ++i) {
+  for (int i{0}; i < branching_factor_ + 1; ++i) {
     children_.push_back(nullptr);
   }
 }
 
-int Node::get_best_action() const
+int Node::explore_best_action() const
 {
   int best_action{0};
   double best_action_ucb1_value = get_ucb1_bound(best_action, exploration_bonus_, N_s_a_, Q_s_a_);
   for (int i = 0; i < branching_factor_; ++i) {
+    if (i == id_) {
+      continue;
+    }
+
     double ith_ucb1_value = get_ucb1_bound(i, exploration_bonus_, N_s_a_, Q_s_a_);
     if (ith_ucb1_value > best_action_ucb1_value) {
       best_action = i;
@@ -30,6 +36,37 @@ int Node::get_best_action() const
     }
   }
   return best_action;
+}
+
+bool Node::has_been_visited()
+{
+  if (!node_has_been_visited_) {
+    node_has_been_visited_ = true;
+    return false;
+  }
+  return true;
+}
+
+std::shared_ptr<Node> Node::take_action(const int action)
+{
+  if (action >= branching_factor_ || action < 0 || action == id_) {
+    return shared_from_this();
+  }
+
+  if (children_.at(action) == nullptr) {
+    children_[action] = std::make_shared<Node>(action, branching_factor_, exploration_bonus_);
+  }
+  return children_.at(action);
+}
+
+void Node::update_count_and_action_value_function(const int best_action, const double q)
+{
+  if (best_action >= branching_factor_ || best_action < 0) {
+    return;
+  }
+
+  N_s_a_[best_action] += 1;
+  Q_s_a_[best_action] = compute_running_average(q, Q_s_a_[best_action], N_s_a_[best_action]);
 }
 
 double get_ucb1_bound(const int & action, const double & exploration_bonus,
@@ -47,22 +84,76 @@ double get_ucb1_bound(const int & action, const double & exploration_bonus,
   return Q_s_a[action] + exploration_bonus * std::sqrt(std::log(N) / N_s_a[action]);
 }
 
-MonteCarloTreeSearch::MonteCarloTreeSearch(int num_states, int num_actions)
-    : num_states_{num_states}
-    , num_actions_{num_actions}
-{}
-
-int MonteCarloTreeSearch::monte_carlo_tree_search(const int curr_state, const uint num_iter,
-                                                  const uint depth)
+double compute_running_average(const int new_val, const int old_val, const int new_count)
 {
-  for (uint i = 0; i < num_iter; ++i) {
-    simulate();
-  }
-
-  return get_greedy_action(curr_state);
+  return old_val + (new_val - old_val) / new_count;
 }
 
-void MonteCarloTreeSearch::simulate() { return; }
-int MonteCarloTreeSearch::get_greedy_action(const int curr_state) { return 0; }
+MonteCarloTreeSearch::MonteCarloTreeSearch(const int num_states, const int num_actions,
+                                           const double discount_factor)
+    : num_states_{num_states}
+    , num_actions_{num_actions}
+    , discount_factor_{discount_factor}
+{}
+
+int MonteCarloTreeSearch::monte_carlo_tree_search(const int initial_state, const uint num_iter,
+                                                  const uint depth, const double exploration_bonus)
+{
+  if (initial_state >= num_states_) {
+    return -1;
+  }
+
+  auto initial_node = std::make_shared<Node>(initial_state, num_actions_, exploration_bonus);
+  for (uint i = 0; i < num_iter; ++i) {
+    simulate(initial_node, depth);
+  }
+  return find_greedy_action(initial_node);
+}
+
+double MonteCarloTreeSearch::simulate(std::shared_ptr<Node> curr_state, const uint & depth)
+{
+  if (depth <= 0) {
+    return compute_value_function_estimate(curr_state);
+  }
+
+  if (!curr_state->has_been_visited()) {
+    return compute_value_function_estimate(curr_state);
+  }
+
+  int best_action = curr_state->explore_best_action();
+
+  std::shared_ptr<Node> next_state = transition_from_state(curr_state, best_action);
+  double r = compute_reward_from_transitioning(curr_state, next_state);
+
+  double q = r + discount_factor_ * simulate(next_state, depth - 1);
+
+  curr_state->update_count_and_action_value_function(best_action, q);
+
+  return q;
+}
+
+int MonteCarloTreeSearch::find_greedy_action(const std::shared_ptr<Node> curr_state) const
+{
+  // TODO:
+  return 0;
+}
+
+double compute_value_function_estimate(const std::shared_ptr<Node> curr_state)
+{
+  // TODO: optional lookahead with rollout
+  return 0;
+}
+
+std::shared_ptr<Node> transition_from_state(std::shared_ptr<Node> curr_node, const int action)
+{
+  return curr_node->take_action(action);
+}
+
+double compute_reward_from_transitioning(const std::shared_ptr<Node> curr_state,
+                                         const std::shared_ptr<Node> next_state)
+{
+  // TODO:
+  return 0;
+}
 
 } // namespace eyes_on_guys
